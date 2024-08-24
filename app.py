2e1f62848739b9af998464776df6f581a2e51a01
matias torres <matias.torres1812@gmail.com>
2024-08-24 12:04:45 -0400

    .

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ async def get_places(
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
